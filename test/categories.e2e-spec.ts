b075a1456738856933c5c5056db56dbfb1744e3a
Nguyen Duc Vi <nducvi.information@gmail.com>
2026-05-12 22:05:20 +0700

    cicd, testing

---
+++ test/categories.e2e-spec.ts
@@ -0,0 +1,30 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Categories (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/api/v1/categories (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/api/v1/categories')
+      .expect(200)
+      .then(response => {
+        expect(Array.isArray(response.body)).toBe(true);
+      });
+  });
+});
