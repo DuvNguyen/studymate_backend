6479445ae6c2cf58f3c6324ce7c0fafc1de60f0b
Nguyen Duc Vi <nducvi.information@gmail.com>
2026-05-14 22:13:12 +0700

    fix build progess

--- src/modules/carts/carts.service.spec.ts
+++ src/modules/carts/carts.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { CartsService } from './carts.service';
+import { Cart } from '../../database/entities/cart.entity';
+import { CartItem } from '../../database/entities/cart-item.entity';
+import { Course } from '../../database/entities/course.entity';
+import { Enrollment } from '../../database/entities/enrollment.entity';
 
 describe('CartsService', () => {
   let service: CartsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CartsService],
+      providers: [
+        CartsService,
+        { provide: getRepositoryToken(Cart), useValue: {} },
+        { provide: getRepositoryToken(CartItem), useValue: {} },
+        { provide: getRepositoryToken(Course), useValue: {} },
+        { provide: getRepositoryToken(Enrollment), useValue: {} },
+      ],
     }).compile();
 
     service = module.get<CartsService>(CartsService);
